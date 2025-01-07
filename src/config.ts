762af8c1d8a4dce495c55306509729165620caa5
surya-pype <suryadipta@pypeai.com>
2025-01-07 14:15:14 +0530

    minor changes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   DEFAULT_DASHBOARD_URL: "https://app.pypeai.com/api/analytics",
   DEFAULT_PROMPT_VERSIONS_URL: "https://app.pypeai.com/api/prompt-version",
+  // DEFAULT_DASHBOARD_URL: "http://localhost:3000/api/analytics",
 };

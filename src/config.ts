e6c82e570c67c0631cce0926637c4df3cecde942
surya-pype <suryadipta@pypeai.com>
2025-01-07 14:09:00 +0530

    prod url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  DEFAULT_DASHBOARD_URL: 'http://localhost:3000/api/analytics',
-  DEFAULT_PROMPT_VERSIONS_URL: 'http://localhost:3000/api/prompt-versions',
+  DEFAULT_DASHBOARD_URL: "https://app.pypeai.com/api/analytics",
+  DEFAULT_PROMPT_VERSIONS_URL: "https://app.pypeai.com/api/prompt-version",
 };
